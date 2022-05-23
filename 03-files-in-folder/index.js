55ebfe5ce4f5e79157aa78b27dc649f7ae40225f
Iryna Pliakhanava <irkapuh@gmail.com>
2022-05-23 14:58:17 +0300

    feat: solve fourth problem

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,27 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true },
-    (err, files) => {
+fs.readdir(
+  path.join(__dirname, 'secret-folder'),
+  { withFileTypes: true },
+  (err, files) => {
     console.log('\nCurrent directory filenames:');
     if (err) {
-        console.log(err);
+      console.log(err);
     } else {
-        files.forEach(file => {
-            console.log(file);
-            console.log(file.isFile());
-            fs.stat(path.join(__dirname, 'secret-folder', file.name),
-            (err, stats) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(stats);
-                }
-                }
-            );
-        });
+      files.forEach((file) => {
+        console.log(file);
+        console.log(file.isFile());
+        fs.stat(
+          path.join(__dirname, 'secret-folder', file.name),
+          (err, stats) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(stats);
+            }
+          }
+        );
+      });
     }
-});
-
-
-
+  }
+);
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const fsPromises = fs.promises;
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, (err) => {
+  if (err) throw err;
+  console.log('Папка была создана');
+});
+
+fs.readdir(
+  path.join(__dirname, 'files'),
+  { withFileTypes: true },
+  (err, files) => {
+    console.log('\nCurrent directory filenames:');
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach((file) => {
+        console.log(file);
+        fs.copyFile(
+          path.join(__dirname, 'files', file.name),
+          path.join(__dirname, 'files-copy', file.name),
+          (err) => {
+            if (err) throw err;
+            console.log('Files Copied');
+          }
+        );
+      });
+    }
+  }
+);
